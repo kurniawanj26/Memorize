494e64e9d129a880f5fe199b294a9f390a2ff92b
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2024-10-29 21:24:09 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jayadi Kurniawan on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
